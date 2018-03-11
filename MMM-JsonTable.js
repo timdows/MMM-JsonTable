16bf1f08b9f4634b814d4cddb0dbdc0b2f3f6770
Timdows <tim@inexpro.nl>
2018-03-11 21:54:29 +0100

    Get json from configured url

--- MMM-JsonTable.js
+++ MMM-JsonTable.js
@@ -4,7 +4,31 @@ Module.register("MMM-JsonTable", {
 
 	// Default module config.
 	defaults: {
-		text: "Hello World!"
+		text: "Hello World!",
+		url: "",
+		updateInterval: 15000
+	},
+
+	start: function () {
+		this.getJson();
+		this.scheduleUpdate();
+	},
+
+	scheduleUpdate: function () {
+		var self = this;
+		setInterval(function () {
+			self.getJson();
+		}, this.config.updateInterval);
+	},
+
+	getJson: function () {
+		this.sendSocketNotification('GET_JSON', this.config.url);
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "JSON_RESULT") {
+			console.log(payload);
+		}
 	},
 
 	// Override dom generator.
