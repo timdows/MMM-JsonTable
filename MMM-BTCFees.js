00fd832f6cae0c2755ee3e808f16e0920bce06df
Nikola <n@bobetic.com>
2021-10-16 23:45:04 +0200

    adjust price to multiply satoshi with btc..

--- MMM-BTCFees.js
+++ MMM-BTCFees.js
@@ -168,7 +168,7 @@ Module.register("MMM-BTCFees", {
 		if (typeof input === "number" ) {
 			
 			let price = 0;
-			price = (input*this.btcData).toFixed(2);
+			price = (input/1000000*140*this.btcData).toFixed(2);
 			return price;
 		}
 		else {
