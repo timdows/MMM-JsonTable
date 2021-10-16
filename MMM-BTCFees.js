7b4b63106f08f226d17ef75bcf92601358bb92af
Nikola <n@bobetic.com>
2021-10-16 19:29:48 +0200

    dev-test1.4

--- MMM-BTCFees.js
+++ MMM-BTCFees.js
@@ -3,10 +3,12 @@
 Module.register("MMM-BTCFees", {
 
 	jsonData: "",
+	btcData: 0,
 
 	// Default module config.
 	defaults: {
 		url: "https://mempool.space/api/v1/fees/recommended",
+		urlBtc: "https://api.cryptonator.com/api/ticker/btc-usd",
 		arrayName: null,
 		keepColumns: [],
 		size: 0,
@@ -32,6 +34,15 @@ Module.register("MMM-BTCFees", {
 		this.sendSocketNotification("MMM-BTCFees_GET_JSON", this.config.url);
 	},
 
+	getBtc: function () {
+		this.sendSocketNotification("MMM-BTCFees_GET_BTC", this.config.urlBtc);
+	},
+	btcToUsd: function (value) {
+		var price= 0;
+		price = (value*getBtc(urlBtc)).toFixed(2);
+		return price;
+	},
+
 	socketNotificationReceived: function (notification, payload) {
 		if (notification === "MMM-BTCFees_JSON_RESULT") {
 			// Only continue if the notification came from the request we made
@@ -39,6 +50,17 @@ Module.register("MMM-BTCFees", {
 			if (payload.url === this.config.url)
 			{
 				this.jsonData = payload.data;
+				this.updateDom(500);
+			}
+		}
+	},
+	socketNotificationBtcReceived: function (notification, payload) {
+		if (notification === "MMM-BTCFees_BTC_RESULT") {
+			// Only continue if the notification came from the request we made
+			// This way we can load the module more than once
+			if (payload.url === this.config.urlBtc)
+			{
+				this.btcData = payload.data;
 				this.updateDom(500);
 			}
 		}
@@ -105,8 +127,6 @@ Module.register("MMM-BTCFees", {
 			else {
 				if ( this.config.keepColumns.length == 0 || this.config.keepColumns.indexOf(key) >= 0 ){
 					valueToDisplay = jsonObject[key];
-				//	Log.info('key in 107'+key);  
-				//	Log.info('jsonObject in 108'+jsonObject);  
 					nameToDisplay = key;
 
 				}
@@ -114,31 +134,38 @@ Module.register("MMM-BTCFees", {
 
 			var cellText = document.createTextNode(valueToDisplay);
 			var cellName = document.createTextNode(nameToDisplay);
+			var cellUsd = document.createTextNode(btcToUsd(valueToDisplay));
 
 			if ( this.config.size > 0 && this.config.size < 9 ){
 				var h = document.createElement("H" + this.config.size );
 				h.appendChild(cellText)
 				var h2 = document.createElement("H" + this.config.size );
 				h2.appendChild(cellName)
+				var h3 = document.createElement("H" + this.config.size );
+				h3.appendChild(cellUsd)
 				cell.appendChild(h);
 				cell2.appendChild(h2);
+				cell3.appendChild(h3);
 			}
 			else
 			{
 				cell.appendChild(cellText);
 				cell2.appendChild(cellName);
+				cell3.appendChild(cellUsd);
 			}
 
 			row.appendChild(cell2);
 			row.appendChild(cell);
+			row.appendChild(cell3);
 		}
 		return row;
 	},
 
-	// Format a date string or return the input
-	getFormattedValue: function (input) {
+
+	//get BTC price
+	getBtcValueInUsd: function (input) {
 		var m = moment(input);
-		if (typeof input === "string" && m.isValid()) {
+		if (typeof input === "number" && m.isValid()) {
 			// Show a formatted time if it occures today
 			if (m.isSame(new Date(), "day") && m.hours() !== 0 && m.minutes() !== 0 && m.seconds() !== 0) {
 				return m.format("HH:mm:ss");
--- node_helper.js
+++ node_helper.js
@@ -15,11 +15,25 @@ module.exports = NodeHelper.create({
 		});
 
 	},
+	getBtc: function (url) {
+		var self = this;
+
+		fetch(url).then(response => response.json()).then(json => {
+			// Send the json data back with the url to distinguish it on the receiving part
+			self.sendSocketNotification("MMM-BTCFees_BTC_RESULT", {url: url, data: json.ticker.price});
+		});
+
+	},
 
 	//Subclass socketNotificationReceived received.
 	socketNotificationReceived: function (notification, url) {
 		if (notification === "MMM-BTCFees_GET_JSON") {
 			this.getJson(url);
+		}
+	},
+	socketNotificationBtcReceived: function (notification, url) {
+		if (notification === "MMM-BTCFees_GET_BTC") {
+			this.getBtc(url);
 		}
 	}
 });
