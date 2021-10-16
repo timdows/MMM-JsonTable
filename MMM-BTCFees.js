95f1bdc27ffc285381ba801155b7691c3858754f
Nikola <n@bobetic.com>
2021-10-16 22:35:28 +0200

    update

--- MMM-BTCFees.js
+++ MMM-BTCFees.js
@@ -131,7 +131,7 @@ Module.register("MMM-BTCFees", {
 			var cellText = document.createTextNode(valueToDisplay);
 			var cellName = document.createTextNode(nameToDisplay);
 			//var cellUsd = document.createTextNode(valueToDisplay);//test without function
-			var cellUsd = document.createTextNode(btcToUsd(valueToDisplay));
+			var cellUsd = document.createTextNode(this.btcToUsd(valueToDisplay));
 
 			if ( this.config.size > 0 && this.config.size < 9 ){
 				var h = document.createElement("H" + this.config.size );
