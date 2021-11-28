39b0a20868ca7c3e70e08da9d39f92d9e9c0184d
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2021-11-28 18:31:54 +0100

    Replace tabs

--- MMM-JsonTable.js
+++ MMM-JsonTable.js
@@ -2,143 +2,141 @@
 
 Module.register("MMM-JsonTable", {
 
-	jsonData: null,
+  jsonData: null,
 
-	// Default module config.
-	defaults: {
-		url: "",
-		arrayName: null,
-		keepColumns: [],
-		size: 0,
-		tryFormatDate: false,
-		updateInterval: 15000,
-		animationSpeed: 500,
-		descriptiveRow: null
-	},
+  // Default module config.
+  defaults: {
+    url: "",
+    arrayName: null,
+    keepColumns: [],
+    size: 0,
+    tryFormatDate: false,
+    updateInterval: 15000,
+    animationSpeed: 500,
+    descriptiveRow: null
+  },
 
-	start: function () {
-		this.getJson();
-		this.scheduleUpdate();
-	},
+  start: function () {
+    this.getJson();
+    this.scheduleUpdate();
+  },
 
-	scheduleUpdate: function () {
-		var self = this;
-		setInterval(function () {
-			self.getJson();
-		}, this.config.updateInterval);
-	},
+  scheduleUpdate: function () {
+    var self = this;
+    setInterval(function () {
+      self.getJson();
+    }, this.config.updateInterval);
+  },
 
-	// Request node_helper to get json from url
-	getJson: function () {
-		this.sendSocketNotification("MMM-JsonTable_GET_JSON", this.config.url);
-	},
+  // Request node_helper to get json from url
+  getJson: function () {
+    this.sendSocketNotification("MMM-JsonTable_GET_JSON", this.config.url);
+  },
 
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "MMM-JsonTable_JSON_RESULT") {
-			// Only continue if the notification came from the request we made
-			// This way we can load the module more than once
-			if (payload.url === this.config.url)
-			{
-				this.jsonData = payload.data;
-				this.updateDom(this.config.animationSpeed);
-			}
-		}
-	},
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "MMM-JsonTable_JSON_RESULT") {
+      // Only continue if the notification came from the request we made
+      // This way we can load the module more than once
+      if (payload.url === this.config.url) {
+        this.jsonData = payload.data;
+        this.updateDom(this.config.animationSpeed);
+      }
+    }
+  },
 
-	// Override dom generator.
-	getDom: function () {
-		var wrapper = document.createElement("div");
-		wrapper.className = "xsmall";
+  // Override dom generator.
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    wrapper.className = "xsmall";
 
-		if (!this.jsonData) {
-			wrapper.innerHTML = "Awaiting json data...";
-			return wrapper;
-		}
-		
-		var table = document.createElement("table");
-		var tbody = document.createElement("tbody");
-		
-		var items = [];
-		if (this.config.arrayName) {
-			items = this.jsonData[this.config.arrayName];
-		}
-		else {
-			items = this.jsonData;
-		}
+    if (!this.jsonData) {
+      wrapper.innerHTML = "Awaiting json data...";
+      return wrapper;
+    }
 
-		// Check if items is of type array
-		if (!(items instanceof Array)) {
-			wrapper.innerHTML = "Json data is not of type array! " +
-				"Maybe the config arrayName is not used and should be, or is configured wrong";
-			return wrapper;
-		}
+    var table = document.createElement("table");
+    var tbody = document.createElement("tbody");
 
-		items.forEach(element => {
-			var row = this.getTableRow(element);
-			tbody.appendChild(row);
-		});
-		
-		// Add in Descriptive Row Header
-		if (this.config.descriptiveRow) {
-			var header = table.createTHead();
-			header.innerHTML = this.config.descriptiveRow;
-		}
+    var items = [];
+    if (this.config.arrayName) {
+      items = this.jsonData[this.config.arrayName];
+    }
+    else {
+      items = this.jsonData;
+    }
 
-		table.appendChild(tbody);
-		wrapper.appendChild(table);
-		return wrapper;
-	},
+    // Check if items is of type array
+    if (!(items instanceof Array)) {
+      wrapper.innerHTML = "Json data is not of type array! " +
+        "Maybe the config arrayName is not used and should be, or is configured wrong";
+      return wrapper;
+    }
 
-	getTableRow: function (jsonObject) {
-		var row = document.createElement("tr");
-		for (var key in jsonObject) {
-			var cell = document.createElement("td");
-			
-			var valueToDisplay = "";
-			if (key === "icon") {
-				cell.classList.add("fa", jsonObject[key]);
-			}
-			else if (this.config.tryFormatDate) {
-				valueToDisplay = this.getFormattedValue(jsonObject[key]);
-			}
-			else {
-				if ( this.config.keepColumns.length == 0 || this.config.keepColumns.indexOf(key) >= 0 ){
-					valueToDisplay = jsonObject[key];
-				}
-			}
+    items.forEach(element => {
+      var row = this.getTableRow(element);
+      tbody.appendChild(row);
+    });
 
-			var cellText = document.createTextNode(valueToDisplay);
+    // Add in Descriptive Row Header
+    if (this.config.descriptiveRow) {
+      var header = table.createTHead();
+      header.innerHTML = this.config.descriptiveRow;
+    }
 
-			if ( this.config.size > 0 && this.config.size < 9 ){
-				var h = document.createElement("H" + this.config.size );
-				h.appendChild(cellText)
-				cell.appendChild(h);
-			}
-			else
-			{
-				cell.appendChild(cellText);
-			}
+    table.appendChild(tbody);
+    wrapper.appendChild(table);
+    return wrapper;
+  },
 
-			row.appendChild(cell);
-		}
-		return row;
-	},
+  getTableRow: function (jsonObject) {
+    var row = document.createElement("tr");
+    for (var key in jsonObject) {
+      var cell = document.createElement("td");
 
-	// Format a date string or return the input
-	getFormattedValue: function (input) {
-		var m = moment(input);
-		if (typeof input === "string" && m.isValid()) {
-			// Show a formatted time if it occures today
-			if (m.isSame(new Date(), "day") && m.hours() !== 0 && m.minutes() !== 0 && m.seconds() !== 0) {
-				return m.format("HH:mm:ss");
-			}
-			else {
-				return m.format("YYYY-MM-DD");
-			}
-		}
-		else {
-			return input;
-		}
-	}
+      var valueToDisplay = "";
+      if (key === "icon") {
+        cell.classList.add("fa", jsonObject[key]);
+      }
+      else if (this.config.tryFormatDate) {
+        valueToDisplay = this.getFormattedValue(jsonObject[key]);
+      }
+      else {
+        if (this.config.keepColumns.length == 0 || this.config.keepColumns.indexOf(key) >= 0) {
+          valueToDisplay = jsonObject[key];
+        }
+      }
+
+      var cellText = document.createTextNode(valueToDisplay);
+
+      if (this.config.size > 0 && this.config.size < 9) {
+        var h = document.createElement("H" + this.config.size);
+        h.appendChild(cellText)
+        cell.appendChild(h);
+      }
+      else {
+        cell.appendChild(cellText);
+      }
+
+      row.appendChild(cell);
+    }
+    return row;
+  },
+
+  // Format a date string or return the input
+  getFormattedValue: function (input) {
+    var m = moment(input);
+    if (typeof input === "string" && m.isValid()) {
+      // Show a formatted time if it occures today
+      if (m.isSame(new Date(), "day") && m.hours() !== 0 && m.minutes() !== 0 && m.seconds() !== 0) {
+        return m.format("HH:mm:ss");
+      }
+      else {
+        return m.format("YYYY-MM-DD");
+      }
+    }
+    else {
+      return input;
+    }
+  }
 
 });
--- node_helper.js
+++ node_helper.js
@@ -2,24 +2,23 @@ var NodeHelper = require('node_helper');
 var fetch = require('node-fetch');
 
 module.exports = NodeHelper.create({
-	start: function () {
-		console.log('MMM-JsonTable helper started...');
-	},
+  start: function () {
+    console.log('MMM-JsonTable helper started...');
+  },
 
-	getJson: function (url) {
-		var self = this;
+  getJson: function (url) {
+    var self = this;
 
-		fetch(url).then(response => response.json()).then(json => {
-			// Send the json data back with the url to distinguish it on the receiving part
-			self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: json});
-		});
+    fetch(url).then(response => response.json()).then(json => {
+      // Send the json data back with the url to distinguish it on the receiving part
+      self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: json});
+    });
+  },
 
-	},
-
-	//Subclass socketNotificationReceived received.
-	socketNotificationReceived: function (notification, url) {
-		if (notification === "MMM-JsonTable_GET_JSON") {
-			this.getJson(url);
-		}
-	}
+  //Subclass socketNotificationReceived received.
+  socketNotificationReceived: function (notification, url) {
+    if (notification === "MMM-JsonTable_GET_JSON") {
+      this.getJson(url);
+    }
+  }
 });
