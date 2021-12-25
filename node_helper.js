4f48735b1e933828437245587eedd82cf5700565
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2021-12-25 13:00:11 +0100

    Linting

--- node_helper.js
+++ node_helper.js
@@ -1,18 +1,23 @@
-let NodeHelper = require('node_helper');
-let fetch = require('node-fetch');
+let NodeHelper = require("node_helper");
+let fetch = require("node-fetch");
 
 module.exports = NodeHelper.create({
   start: function () {
-    console.log('MMM-JsonTable helper started...');
+    console.log("MMM-JsonTable helper started...");
   },
 
   getJson: function (url) {
     let self = this;
 
-    fetch(url).then(response => response.json()).then(json => {
-      // Send the json data back with the url to distinguish it on the receiving part
-      self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {url: url, data: json});
-    });
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        // Send the json data back with the url to distinguish it on the receiving part
+        self.sendSocketNotification("MMM-JsonTable_JSON_RESULT", {
+          url: url,
+          data: json
+        });
+      });
   },
 
   //Subclass socketNotificationReceived received.
