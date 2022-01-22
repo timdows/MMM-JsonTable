4eddc2a050ea00cde38a6657af9df8eba2811a67
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-01-22 01:13:29 +0100

    Add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
